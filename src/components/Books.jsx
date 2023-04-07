dfff7341e19ac7e46e0b1beb99827b0e88ad6991
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-07 15:53:17 +0600

    Book cards are shown in book tab

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Book from "./Book";
 
 const Books = () => {
+  const { books } = useLoaderData();
+  console.log(books);
   return (
-    <div>
-      <h1>books</h1>
+    <div className="my-container">
+      <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">
+        {books.map((book) => (
+          <Book key={book.isbn13} book={book} />
+        ))}
+      </div>
     </div>
   );
 };
