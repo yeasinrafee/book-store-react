974b21a8a3d2f5caa1a0b2d6ed79783b779d2a27
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-07 08:17:53 +0600

    Navlink added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to nextPage!</h1>
+      <Header />
       <Outlet />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { BoltIcon } from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="bg-gray-100 flex items-center justify-between">
+      <Link to={`/`} className="inline-flex items-center">
+        <BoltIcon className="h-6 w-6 text-blue-500" />
+        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+          nextPage
+        </span>
+      </Link>
+
+      {/* Nav Items Section */}
+      <ul className="items-center hidden space-x-8 lg:flex">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/books"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            Books
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active" : "default")}
+          >
+            About us
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
